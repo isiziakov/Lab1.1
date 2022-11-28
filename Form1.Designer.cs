5db6ff309d40ec8a0adf2cf9075dfe073ddea761
isiziakov <ivan.siziakov@mail.ru>
2022-11-28 22:12:28 +0300

    Добавьте файлы проекта.

---
+++ Form1.Designer.cs
@@ -0,0 +1,206 @@
+﻿namespace Lab1._1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.button4 = new System.Windows.Forms.Button();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(97, 25);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Вершин:";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "2",
+            "3",
+            "4",
+            "5",
+            "6",
+            "7",
+            "8",
+            "9",
+            "10"});
+            this.comboBox1.Location = new System.Drawing.Point(115, 6);
+            this.comboBox1.MaxDropDownItems = 9;
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(53, 33);
+            this.comboBox1.TabIndex = 1;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            this.comboBox1.TextUpdate += new System.EventHandler(this.comboBox1_TextUpdate);
+            // 
+            // button4
+            // 
+            this.button4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button4.Location = new System.Drawing.Point(65, 383);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(106, 31);
+            this.button4.TabIndex = 16;
+            this.button4.Text = "Рассчитать";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // textBox4
+            // 
+            this.textBox4.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBox4.Location = new System.Drawing.Point(17, 144);
+            this.textBox4.Multiline = true;
+            this.textBox4.Name = "textBox4";
+            this.textBox4.ReadOnly = true;
+            this.textBox4.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBox4.Size = new System.Drawing.Size(212, 233);
+            this.textBox4.TabIndex = 15;
+            // 
+            // button3
+            // 
+            this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button3.Location = new System.Drawing.Point(123, 107);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(106, 31);
+            this.button3.TabIndex = 14;
+            this.button3.Text = "Очистить";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // textBox3
+            // 
+            this.textBox3.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBox3.Location = new System.Drawing.Point(17, 107);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(100, 31);
+            this.textBox3.TabIndex = 13;
+            this.textBox3.Visible = false;
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button2.Location = new System.Drawing.Point(123, 70);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(106, 31);
+            this.button2.TabIndex = 12;
+            this.button2.Text = "Добавить";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBox2.Location = new System.Drawing.Point(17, 70);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 31);
+            this.textBox2.TabIndex = 11;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(12, 42);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(350, 25);
+            this.label2.TabIndex = 10;
+            this.label2.Text = "Ребра[1 вершина 2 вершина вес]:";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBox1.Location = new System.Drawing.Point(368, 42);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBox1.Size = new System.Drawing.Size(420, 233);
+            this.textBox1.TabIndex = 17;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label3.Location = new System.Drawing.Point(363, 9);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(121, 25);
+            this.label3.TabIndex = 18;
+            this.label3.Text = "Результат:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label2;
+        public System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,243 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Lab1._1
+{
+    public partial class Form1 : Form
+    {
+        public IStringParse parse = new StringParse();
+        public class Edge
+        {
+            public int a, b, weight;
+            public Edge(int a, int b, int weight)
+            {
+                this.a = a;
+                this.b = b;
+                this.weight = weight;
+            }
+        }
+        public int size = 2;
+        int weight;
+        public int[] points = new int[10];
+        public List<Edge> edges = new List<Edge>();
+        public string result = "";
+        public int resultW = 0;
+        public Form1()
+        {
+            InitializeComponent();
+            comboBox1.SelectedIndex = 0;
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            size = comboBox1.SelectedIndex + 2;
+            bool[] points = new bool[10];
+            edges = new List<Edge>();
+            weight = 0;
+            textBox4.Text = "";
+            result = "";
+            textBox1.Text = "";
+        }
+
+        private void comboBox1_TextUpdate(object sender, EventArgs e)
+        {
+            bool[] points = new bool[10];
+            edges = new List<Edge>();
+            weight = 0;
+            textBox4.Text = "";
+            result = "";
+            if (int.TryParse(comboBox1.Text, out int a))
+            {
+                int n = int.Parse(comboBox1.Text);
+                if (!(n < 2 || n > 10))
+                {
+                    size = n;
+                    return;
+                }
+            }
+            comboBox1.SelectedIndex = 0;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            add(textBox2.Text);
+        }
+
+        public int addCounter = 0;
+
+        public void add(string text)
+        {
+            var res = parse.Parse(text);
+            if (res != -1)
+            {
+                int a = res / 100;
+                int b = res / 10 % 10;
+                int w = res % 10;
+                if (a > 0 && a <= size && b > 0 && b <= size && w > 0 && a != b)
+                {
+                    edges.Add(new Edge(a, b, w));
+                    addCounter = 0;
+                    for (int i = edges.Count - 1; i > 0; i--)
+                    {
+                        addCounter++;
+                        if (edges[i - 1].weight > edges[i].weight)
+                        {
+                            var buf = edges[i];
+                            edges[i] = edges[i - 1];
+                            edges[i - 1] = buf;
+                        }
+                        else
+                        {
+                            break;
+                        }
+                    }
+                    textBox4.Text += textBox2.Text + "\r\n";
+                }
+            }
+            textBox2.Text = "";
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            edges = new List<Edge>();
+            textBox4.Text = "";
+            /*var numbers = textBox3.Text.Split(' ');
+            if (numbers.Length == 3)
+            {
+                try
+                {
+                    int a = int.Parse(numbers[0]);
+                    int b = int.Parse(numbers[1]);
+                    int w = int.Parse(numbers[2]);
+                    if (a > 1 && a <= size && b > 1 && b <= size && w > 0 && a != b)
+                    {
+                        for (int i = 0; i < edges.Count; i++)
+                        {
+                            if (edges[i].a == a && edges[i].b == b && edges[i].weight == w)
+                            {
+                                edges.RemoveAt(i);
+                                break;
+                            }
+                        }
+                    }
+                    var index = textBox4.Text.IndexOf(textBox3.Text);
+                    textBox4.Text = textBox4.Text.Remove(index, textBox3.Text.Length + 2);
+                }
+                catch (Exception)
+                {
+
+                }
+                finally
+                {
+                    textBox3.Text = "";
+                }
+            }*/
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            calculate();
+        }
+
+        public int count = 0;
+        public void calculate()
+        {
+            count = 0;
+            resultW = 0;
+            result = "";
+            points = new int[size];
+            for (int i = 0; count < size - 1 && i < edges.Count; i++)
+            {
+                count = addEdge(count, i);
+            }
+            if (count != size - 1)
+            {
+                textBox1.Text = "Невозможно построить остовное дерево";
+            }
+            else
+            {
+                textBox1.Text = result + "\r\n" + resultW;
+            }
+        }
+
+        public int addEdge(int count, int i)
+        {
+            addCounter = 0;
+            if (points[edges[i].a - 1] == 0 && points[edges[i].b - 1] == 0)
+            {
+                count++;
+                result += edges[i].a + "-" + edges[i].b + "\r\n";
+                resultW += edges[i].weight;
+                points[edges[i].b - 1] = i + 1;
+                points[edges[i].a - 1] = i + 1;
+            }
+            else
+            {
+                if (points[edges[i].a - 1] == 0 || points[edges[i].b - 1] == 0)
+                {
+                    count++;
+                    result += edges[i].a + "-" + edges[i].b + "\r\n";
+                    resultW += edges[i].weight;
+                    if (points[edges[i].a - 1] == 0)
+                    {
+                        points[edges[i].a - 1] = points[edges[i].b - 1];
+                    }
+                    else
+                    {
+                        points[edges[i].b - 1] = points[edges[i].a - 1];
+                    }
+                }
+                else
+                {
+                    if (points[edges[i].a - 1] != points[edges[i].b - 1])
+                    {
+                        var buf = points[edges[i].b - 1];
+                        for (int j = 0; j < size; j++)
+                        {
+                            addCounter++;
+                            if (points[j] == buf)
+                            {
+                                points[j] = points[edges[i].a - 1];
+                            }
+                        }
+                        result += edges[i].a + "-" + edges[i].b + "\r\n";
+                        resultW += edges[i].weight;
+                    }
+                }
+            }
+            return count;
+        }
+    }
+    public interface IStringParse
+    {
+        int Parse(string numbers);
+    }
+
+    public class StringParse : IStringParse
+    {
+        public StringParse()
+        {
+
+        }
+
+        public int Parse(string text)
+        {
+            var numbers = text.Split(' ');
+            if (int.TryParse(numbers[0], out int aa) && int.TryParse(numbers[1], out int bb) && int.TryParse(numbers[2], out int cc))
+            {
+                return int.Parse(numbers[0]) * 100 + int.Parse(numbers[1]) * 10 + int.Parse(numbers[2]);
+            }
+            else
+            {
+                return -1;
+            }
+        }
+    }
+}
